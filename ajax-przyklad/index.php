aad57dc22c86b2f76da0a39b830f80638cceef18
adamwalicki <69089626+adamwalicki@users.noreply.github.com>
2020-08-02 22:01:43 +0200

    Add files via upload

---
+++ ajax-przyklad/index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
+</head>
+<body>
+<form id="loginform" method="post">
+    <p>Username: <input type="text" name="username" id="username" value=""></p>
+    <p>Password: <input type="password" name="password" id="password" value=""></p>
+    <input type="submit" name="loginsub" id="loginsub" value="Login">
+    </form>
+<?php
+
+?>
+<script src="script.js"></script>
+</body>
+</html>
